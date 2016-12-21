aafa76fb81f368655051b391345c820a38297829
Zachary Richards <zachary.j.richards@gmail.com>
2016-12-21 13:15:40 -0800

    add back to top arrow that works

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,11 @@ $(document).ready(function(){
       }
     });
   });
+  $(window).scroll(function() {
+		if ($(this).scrollTop() > 200) {
+			$('#go-top').fadeIn(200);
+		} else {
+			$('#go-top').fadeOut(200);
+		}
+	});
 });
